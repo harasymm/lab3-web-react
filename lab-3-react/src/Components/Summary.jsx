b84234631d2eda94976845b60158ca06e84e26db
harasymm <markiangarasim7328@gmail.com>
2026-05-15 18:19:24 +0300

    Done: Lab3-web Part1. Added: styles.css for htlm. Modified: index.html

--- lab-3-react/src/Components/Summary.jsx
+++ lab-3-react/src/Components/Summary.jsx
@@ -4,6 +4,8 @@ function Summary() {
       <h2>Summary/Про мене</h2>
       <p>
         Motivated and detail-oriented Computer Science and Computer Systems Security student at LPNU. I have experience in web development, database management, and programming.
+      </p>
+      <p>
         Студент Комп'ютерних наук та Безпеки Комп'ютерних Систем Львівської політехніки. Маю досвід в розробці веб-сайтів, уп управлінні базами даних та програмуванні.
       </p>
     </section>
